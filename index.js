dccc1d3f0fbce66ec0b0871e00ac800b2ddd0c0a
Gadiye <gadiyek@gmail.com>
2024-07-08 16:42:40 +0300

    firts commit

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-// Code your solution in this file!
+// index.js
+const companyName = "Scuber";
+
+let mostProfitableNeighborhood = "Chelsea";
+ 
+let companyCeo = "Susan Smith";
+
+const companyLocation = "San Francisco";
+
